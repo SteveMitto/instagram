087d7e37581bba0e1d8abab550715df36c6fac11
Steve <mittosteve@gmail.com>
2019-10-11 23:26:24 +0300

    Creating user signup to allow users to register their accounts

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form
+            form.save()
+            return redirect('home')
+    else:
+        form= UserCreationForm()
+    context={
+    'form':form
+    }
+    return render(request,'registration/signup.html', context)
