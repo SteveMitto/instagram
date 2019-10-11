70ba68a39a36e5cef8d25f37245832d7a527b499
Steve <mittosteve@gmail.com>
2019-10-11 13:05:31 +0300

    Creating a model for the image users upload

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models as md
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(md.Model):
+    image = md.ImageField(upload_to='articles/')
+    name = md.CharField(max_length=70)
+    caption = md.TextField()
+    profile = md.ForeignKey(User,on_delete=md.CASCADE)
+    posted_on = md.DateTimeFiled(auto_now_add=True)
+    def __str__():
+        return f'{name}'
