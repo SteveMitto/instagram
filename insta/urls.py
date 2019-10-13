addfcafdda389f1f31140d0cdc17bdd15390fd81
Steve <mittosteve@gmail.com>
2019-10-13 21:11:23 +0300

    Creating and styling user profile and allowing settings for current user

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',views.home ,name='home'),
     path('signup/',views.signup,name='signup'),
-    path('profile/' , views.profile, name='profile')
+    path('<username>' , views.profile, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Image,Like,Comment,Profile,Tags,Follow
+
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -17,3 +20,18 @@ def signup(request):
     'form':form
     }
     return render(request,'registration/signup.html', context)
+
+def profile(request,username):
+    current_user = User.objects.filter(username = username).first()
+    print(current_user.pk)
+    images = Image.objects.all()
+    following = Follow.objects.filter(follow = current_user)
+    followers = Follow.objects.filter(following = current_user)
+
+    context={
+    'posts':images,
+    'current_user':current_user,
+    'followers':followers,
+    'following':following
+    }
+    return render(request,'profile.html',context)
